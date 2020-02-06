c971f6344a5b4354b863d7d9660c4741f164a7c2
max197 <ma.gomez1@uniandes.edu.co>
2020-02-06 17:35:26 -0500

    testsAñadidos

---
+++ test/test/data_structures/TestQueue.java
@@ -0,0 +1,63 @@
+package test.data_structures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestQueue {
+
+	private Queue cola;
+	private Modelo modelo;
+
+
+
+
+	@Before
+	public void setUp1()
+	{
+
+
+	}
+	@Before
+	public void setUp2()
+	{
+
+
+
+
+	}
+	@Test 
+
+	public void testEnqueue()
+	{
+
+
+	}
+
+	@Test
+	public void testDequeue()
+	{
+
+	}
+
+	@Test 
+	public void testSize()
+	{
+
+	}
+
+	@Test
+	public void testIsEmpty()
+	{
+
+
+	}
+	@Test
+	public void testPeek()
+	{
+
+
+	}
+}
+
---
+++ test/test/data_structures/TestStack.java
@@ -0,0 +1,83 @@
+package test.data_structures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class TestStack
+
+{
+
+
+	private Stack pila;
+	private Modelo modelo;
+
+
+
+	@Before
+	public void setUp1()
+	{
+		
+		
+	}
+	
+	
+	@Before
+	public void setUp2()
+	{
+		
+		
+			
+		
+	}
+	
+	
+	
+	@Test 
+	
+	public void testPush()
+	{
+		
+		
+		
+		
+		
+	}
+	
+	@Test
+	public void testPop()
+	{
+		
+	}
+	
+	@Test 
+	public void testSize()
+	{
+		
+	}
+	
+
+	@Test
+	public void testIsEmpty()
+	{
+
+
+	}
+	@Test
+	public void testPeek()
+	{
+		
+		
+	}
+	
+	
+	
+
+
+
+
+
+}
